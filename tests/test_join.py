92bfbcb7c7df3d60eab5544107d34c971a19361f
Yiping (Allison) <32723567+yiping-allison@users.noreply.github.com>
2026-01-26 12:41:45 -0800

    feat: implement right join

--- tests/test_join.py
+++ tests/test_join.py
@@ -559,3 +559,142 @@ def test_left_join_edge(graph: ig.Graph, y: pd.DataFrame, expected_set: _Expecte
             check_index=False,
             check_names=False,
         )
+
+
+@pytest.mark.parametrize(
+    "y,expected_set",
+    [
+        pytest.param(
+            pd.DataFrame(
+                {
+                    "name": ["d", "e"],
+                }
+            ),
+            _Expected(
+                undirected={
+                    "num_nodes": 2,
+                    "names": ["d", "e"],
+                },
+            ),
+            id="add new nodes",
+        ),
+        pytest.param(
+            pd.DataFrame(
+                {
+                    "name": ["b", "c"],
+                    "color": ["blue", "green"],
+                }
+            ),
+            _Expected(
+                undirected={
+                    "num_nodes": 2,
+                    "names": ["b", "c"],
+                    "attributes": {
+                        "b": {"color": "blue"},
+                        "c": {"color": "green"},
+                    },
+                },
+            ),
+            id="existing nodes with attributes",
+        ),
+    ],
+)
+def test_right_join_node(graph: ig.Graph, y: pd.DataFrame, expected_set: _Expected) -> None:
+    tg = Tidygraph(graph=graph).activate(ActiveType.NODES).join(y, how="right")
+
+    vertex_df = tg.vertex_dataframe
+
+    # We do not separate by undirected vs undirected graphs since nodes are the same in both cases
+    expected = expected_set.undirected
+
+    assert vertex_df["name"].count() == expected["num_nodes"]
+
+    if "attributes" not in expected:
+        return
+
+    expected_attrs = pd.DataFrame(expected["attributes"]).transpose().reset_index(drop=True)
+    attr_cols = [col for col in expected_attrs.columns if col not in ReservedKeywords.NODES]
+    actual_attrs = vertex_df[attr_cols].reset_index(drop=True)
+
+    pd.testing.assert_frame_equal(actual_attrs, expected_attrs)
+
+
+@pytest.mark.parametrize(
+    "y,expected_set",
+    [
+        pytest.param(
+            pd.DataFrame(
+                {
+                    "from": ["a", "a"],
+                    "to": ["b", "c"],
+                }
+            ),
+            _Expected(
+                directed={
+                    "expected_edges": 2,
+                },
+                undirected={
+                    "expected_edges": 2,
+                },
+            ),
+            id="with new edges",
+        ),
+        pytest.param(
+            pd.DataFrame(
+                {
+                    "from": ["b", "c"],
+                    "to": ["c", "a"],
+                    "weight": [1.5, 2.5],
+                }
+            ),
+            _Expected(
+                directed={
+                    "expected_edges": 2,
+                    "attributes": [
+                        {"from": "b", "to": "c", "weight": 1.5},
+                        {"from": "c", "to": "a", "weight": 2.5},
+                    ],
+                },
+                undirected={
+                    "expected_edges": 2,
+                    "attributes": [
+                        {"from": "a", "to": "c", "weight": 2.5},
+                        {"from": "b", "to": "c", "weight": 1.5},
+                    ],
+                },
+            ),
+            id="with attributes on new edges",
+        ),
+    ],
+)
+def test_right_join_edge(graph: ig.Graph, y: pd.DataFrame, expected_set: _Expected) -> None:
+    tg = Tidygraph(graph=graph)
+    tg = tg.activate(ActiveType.EDGES).join(y, how="right")
+    node_df = tg.vertex_dataframe
+    edge_df = tg.edge_dataframe
+
+    # augment the edge dataframe with source and target names for easier testing
+    edge_df["from"] = edge_df["source"].map(lambda x: node_df.loc[x, "name"])
+    edge_df["to"] = edge_df["target"].map(lambda x: node_df.loc[x, "name"])
+
+    expected = expected_set.directed if graph.is_directed() else expected_set.undirected
+
+    assert len(edge_df) == expected["expected_edges"]
+
+    attributes = expected.get("attributes", [])
+    if not attributes:
+        return
+
+    expected_attrs = pd.DataFrame(attributes).reset_index(drop=True)
+    actual_attrs = edge_df.reset_index(drop=True)
+
+    attr_set = expected_attrs.merge(actual_attrs, how="left", on=["from", "to"], suffixes=("_expected", "_actual"))
+
+    attr_cols = [col for col in expected_attrs.columns if col not in ReservedKeywords.EDGES]
+    for col in attr_cols:
+        pd.testing.assert_series_equal(
+            attr_set[f"{col}_expected"],
+            attr_set[f"{col}_actual"],
+            check_index=False,
+            check_names=False,
+        )
