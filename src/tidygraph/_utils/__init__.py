d0f5547ac2196d7a12787c8589cd5e91dd577de2
Yiping (Allison) <32723567+yiping-allison@users.noreply.github.com>
2026-02-05 12:39:22 -0800

    feat: add more centrality functions
    Adds additional centrality functions:
    - harmonic
    - betweenness
    - edge betweenness

--- src/tidygraph/_utils/__init__.py
+++ src/tidygraph/_utils/__init__.py
@@ -1,3 +1,12 @@
+from tidygraph._utils.centrality import (
+    centrality_betweenness,
+    centrality_closeness,
+    centrality_degree,
+    centrality_edge_betweenness,
+    centrality_eigenvector,
+    centrality_harmonic,
+    centrality_pagerank,
+)
 from tidygraph._utils.const import RESERVED_JOIN_KEYWORD, ReservedGraphKeywords
 from tidygraph._utils.join import inner_join, left_join, outer_join, right_join
 from tidygraph._utils.tree import is_forest, is_tree
@@ -5,6 +14,13 @@ from tidygraph._utils.tree import is_forest, is_tree
 __all__ = [
     "RESERVED_JOIN_KEYWORD",
     "ReservedGraphKeywords",
+    "centrality_betweenness",
+    "centrality_closeness",
+    "centrality_degree",
+    "centrality_edge_betweenness",
+    "centrality_eigenvector",
+    "centrality_harmonic",
+    "centrality_pagerank",
     "inner_join",
     "is_forest",
     "is_tree",
